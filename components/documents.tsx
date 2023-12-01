eec858e8c21f65f6875e56e6b0cabda7dabe1afa
Sudhari Sandamini <sudhari08sudh@gmail.com>
2023-12-01 22:41:17 +0530

    document link update

--- components/documents.tsx
+++ components/documents.tsx
@@ -15,24 +15,6 @@ export default function Documentations() {
           {/* Items */}
           <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
 
-            {/* 1st item */}
-            {/* <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
-              <div className="relative flex flex-row items">
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
-                  <path d="M441.412 140.235v338.781c0 18.219-14.778 32.983-32.983 32.983H103.572c-18.219 0-32.983-14.764-32.983-32.983V32.983C70.588 14.764 85.352 0 103.572 0h197.605l140.235 140.235z" fill="#518ef8" data-original="#518ef8"></path>
-                  <path d="M161.18 257.054h189.626v21.071H161.18zM161.18 304.112h189.626v21.071H161.18zM161.18 351.171h189.626v21.071H161.18zM161.18 398.23h134.855v21.071H161.18z" fill="#ffffff" data-original="#ffffff"></path>
-                  <path d="m320.31 137.188 121.102 49.891v-46.844l-68.661-20.273z" fill="#3a5bbc" data-original="#3a5bbc"></path><path d="M441.412 140.235H334.16c-18.22 0-32.983-14.764-32.983-32.983V0l140.235 140.235z" fill="#acd1fc" data-original="#acd1fc"></path></g>
-                </svg>
-                &nbsp;
-                <p className="text-lg text-gray-400 text">Document</p>
-              </div>
-              <h4 className="h4 mb-2">Topic Assessment</h4>
-              <hr/>
-              <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download ✅</a>
-              </div>
-            </div> */}
-
             {/* Project Charter */}
             <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <div className="relative flex flex-row items">
@@ -48,7 +30,7 @@ export default function Documentations() {
               <p></p>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:b:/g/personal/it20049218_my_sliit_lk/EU1OYJshwhlJkyLD3Z-JbG4BurmBJ_9W9EYkwZJ-rW32Fg?e=yC8adU" action="_blank">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:b:/g/personal/it20049218_my_sliit_lk/EU1OYJshwhlJkyLD3Z-JbG4BurmBJ_9W9EYkwZJ-rW32Fg?e=yC8adU" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
@@ -67,11 +49,11 @@ export default function Documentations() {
               <p>📅 2023/03/27    ✅ 6%</p>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:f:/g/personal/it20049218_my_sliit_lk/ErgxHNah6TpNgmjIxHu690AByxKK_4IJg0eBORW36IEWqQ?e=5QXj0l" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
-            {/* 4th item */}
+            {/* Project Proposal PPT */}
             <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <div className="relative flex flex-row items">
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
@@ -86,11 +68,11 @@ export default function Documentations() {
               <p></p>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:p:/g/personal/it20049218_my_sliit_lk/EVXpH1wbEPdKtuhaeYTbPOEBMXFY6K8V1o5vr9LKaxqaAw?e=aI8UX4" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
-            {/* 5th item */} 
+            {/* PP I */} 
             <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <div className="relative flex flex-row items">
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
@@ -105,11 +87,11 @@ export default function Documentations() {
               <p>📅 2023/03/22-26    ✅ 15%</p>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:p:/g/personal/it20049218_my_sliit_lk/EUNVxayNl5xKhA6h1W2goPgBUX9E14vfdZmDagAtpfe1KA?e=KkDB2b">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:p:/g/personal/it20049218_my_sliit_lk/EUNVxayNl5xKhA6h1W2goPgBUX9E14vfdZmDagAtpfe1KA?e=KkDB2b" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
-            {/* 6th item */}
+            {/* Status Documents I */}
             <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <div className="relative flex flex-row items">
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
@@ -123,11 +105,11 @@ export default function Documentations() {
               <h4 className="h4 mb-2">Status Documents I</h4>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:f:/g/personal/it20049218_my_sliit_lk/EpXRuceZ6bRMgN9Hu-4KWzkBDY_54BSBInzC95qUrJcJQg?e=XEnPXy" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
-            {/* 7th item */}
+            {/* PP II */}
             <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <div className="relative flex flex-row items">
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
@@ -142,11 +124,11 @@ export default function Documentations() {
               <p>📅 2023/09/04-06    ✅ 18%</p>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:p:/g/personal/it20049218_my_sliit_lk/EXfM4Pj0EHBIr6gEFsR9mxsBYEAqXKAHMfkVBOerckgpyA?e=Ugnfi8">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:p:/g/personal/it20049218_my_sliit_lk/EXfM4Pj0EHBIr6gEFsR9mxsBYEAqXKAHMfkVBOerckgpyA?e=Ugnfi8" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
-            {/* 8th item */}
+            {/* Status Documents II */}
             <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <div className="relative flex flex-row items">
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
@@ -160,29 +142,11 @@ export default function Documentations() {
               <h4 className="h4 mb-2">Status Documents II</h4>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:f:/g/personal/it20049218_my_sliit_lk/EuNAVRI3XBFGr-IqS1tWEt4B83WF1G-vI6LDJWpzep6Xtg?e=86sLyi" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
-            {/* 9th item */}
-            {/* <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
-              <div className="relative flex flex-row items">
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
-                  <path d="M441.412 140.235v338.781c0 18.219-14.778 32.983-32.983 32.983H103.572c-18.219 0-32.983-14.764-32.983-32.983V32.983C70.588 14.764 85.352 0 103.572 0h197.605l140.235 140.235z" fill="#518ef8" data-original="#518ef8"></path>
-                  <path d="M161.18 257.054h189.626v21.071H161.18zM161.18 304.112h189.626v21.071H161.18zM161.18 351.171h189.626v21.071H161.18zM161.18 398.23h134.855v21.071H161.18z" fill="#ffffff" data-original="#ffffff"></path>
-                  <path d="m320.31 137.188 121.102 49.891v-46.844l-68.661-20.273z" fill="#3a5bbc" data-original="#3a5bbc"></path><path d="M441.412 140.235H334.16c-18.22 0-32.983-14.764-32.983-32.983V0l140.235 140.235z" fill="#acd1fc" data-original="#acd1fc"></path></g>
-                </svg>
-                &nbsp;
-                <p className="text-lg text-gray-400 text">Document</p>
-              </div>
-              <h4 className="h4 mb-2">Research Paper</h4>
-              <hr/>
-              <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download ⬇️</a>
-              </div>
-            </div> */}
-
-            {/* 10th item */}
+            {/* Final Report */}
             <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <div className="relative flex flex-row items">
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
@@ -196,11 +160,11 @@ export default function Documentations() {
               <h4 className="h4 mb-2">Final Report</h4>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:b:/g/personal/it20049218_my_sliit_lk/EakLNFxVzOpMrjzgwrxZ0lMBMxtLOxEDwi4f5Ci6J3Gcmw?e=VSJqAG">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:b:/g/personal/it20049218_my_sliit_lk/EakLNFxVzOpMrjzgwrxZ0lMBMxtLOxEDwi4f5Ci6J3Gcmw?e=VSJqAG" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
-            {/* 11th item */}
+            {/* Final Presentation */}
             <div id="docDiv" className="relative flex flex-col items" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <div className="relative flex flex-row items">
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 512 512"><g>
@@ -215,7 +179,7 @@ export default function Documentations() {
               <p>📅 2023/10/2-26    ✅ 10%</p>
               <hr/>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download ⬇️</a>
+                <a style={{borderRadius: '5px'}} className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://mysliit-my.sharepoint.com/:p:/g/personal/it20049218_my_sliit_lk/Edr286jDQwlIh2atwwlimzIB9oODKijo8TtcXvtQ_EjEhA?e=rsWOjm" target="_blank">Download ⬇️</a>
               </div>
             </div>
 
